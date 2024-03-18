5630b81ccb8229cbe92c5d1a126cd2ee6bd0495f
annetvd <132998548+annetvd@users.noreply.github.com>
2024-03-18 04:00:04 -0600

    Progreso hasta la tercera sección

--- Presentacion/inicio.php
+++ Presentacion/inicio.php
@@ -72,13 +72,13 @@
     </header>
 
     <main>
-        <div style="height: 1500px;" class="container p-0">
+        <div style="height: 3500px;" class="container p-0">
             <div id="about" class="bg-container bg-white">
                 <img id="aboutB" class="background" src="" alt="" />
                 <span class="row p-0 mb-4 px-0 px-xxl-5">
-                    <h2 class="text-center">About the proyect</h2>
-                    <span class="p-5 row justify-content-center gx-5 m-0 bg-content gy-5 pt-3 px-2 px-sm-5 mb-3" height="">
-                        <span class="col-12 col-sm-10 col-md-6 col-xl-5">
+                    <h2 class="text-center">About the<br class="d-sm-none" /> proyect</h2>
+                    <span class="p-5 row justify-content-center gx-5 m-0 bg-content gy-5 pt-0 pt-sm-2 px-2 px-sm-5 mb-1 mb-md-3" height="">
+                        <span class="col-12 col-sm-9 col-md-6 col-xl-5 container-xs px-3 px-sm-4">
                             <div class="shadow-css px-4 pt-5 pb-4 me-0 me-md-1 me-lg-2 bg-white">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon mb-1 mt-2" alt="">
                                     <path fill-rule="evenodd" d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 0 1 .75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 0 1 9.75 22.5a.75.75 0 0 1-.75-.75v-4.131A15.838 15.838 0 0 1 6.382 15H2.25a.75.75 0 0 1-.75-.75 6.75 6.75 0 0 1 7.815-6.666ZM15 6.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" clip-rule="evenodd" />
@@ -88,7 +88,7 @@
                                     and the creation of modules for the creation of statistics and automatic reports.</p>
                             </div>
                         </span>
-                        <span class="col-12 col-sm-10 col-md-6 col-xl-5">
+                        <span class="col-12 col-sm-9 col-md-6 col-xl-5 container-xs px-3 px-sm-4">
                             <div class="shadow-css px-4 pt-5 pb-4 me-0 ms-md-1 ms-lg-2 bg-white">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon mb-1 mt-2" alt="">
                                     <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clip-rule="evenodd" />
@@ -103,11 +103,9 @@
 
             <div class="bg-container blue">
                 <img id="webApp" class="background" src="" alt="" />
-                <span class="row p-0 justify-content-center py-5 my-5 text-white px-lg-4 px-xl-5">
-                    <!-- xl row p-0 justify-content-center py-5 my-5 text-white px-5 -->
-                    <div class="col-auto bg-content pb-3 me-xl-5 ps-4 ps-sm-0 ps-xxl-2 pe-4 pe-sm-0 mb-5 mb-lg-0">
-                        <!-- xl col-4 bg-content pe-5 ps-4 pb-3 ms-2 me-5 || col-4 col-xxl-3 pe-xl-3 ps-4 ps-xl-2-->
-                        <div class="ps-0 ps-lg-2 pb-4 me-xl-5">
+                <span class="row p-0 justify-content-center py-3 py-lg-5 my-5 text-white px-lg-4 px-xl-5">
+                    <div class="col-auto bg-content pb-3 me-xl-5 ps-4 ps-sm-0 ps-xxl-2 pe-4 pe-sm-0 mb-4 mb-sm-5 mb-lg-0">
+                        <div class="ps-2 pb-4 me-xl-5">
                             <h2 class="text-start ps-4 pe-3 pe-sm-0">Business web<br /> application</h2>
                         </div>
                         <div class="translucent-box p-4 mb-1 mb-lg-0">
@@ -117,8 +115,7 @@
                     </div>
                     <div class="w-100 d-inline d-lg-none"></div>
                     <span class="col-auto check-list ps-sm-3 ps-xl-2 bg-content ms-2 ms-sm-3 ms-lg-5 me-sm-3 me-md-4 me-lg-2 me-xl-4">
-                        <!-- col-6 check-list xl -->
-                        <div class="row row-cols-auto">
+                        <div class="row row-cols-auto mt-lg-2">
                             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 106 100" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" alt="">
                                 <use xlink:href="..\Imagenes\start\check.svg#Layer_1" />
                             </svg>
@@ -143,8 +140,8 @@
                             <p>Provide a data analysis service to clients.</p>
                         </div>
                     </span>
-                    <span class="col-auto check-list bg-content ms-2 ms-sm-0 ms-lg-2 ms-xxl-4 mt-4 mt-sm-0">
-                        <div class="row row-cols-auto mt-3 mt-sm-0">
+                    <span class="col-auto check-list bg-content ms-2 ms-sm-0 ms-lg-2 ms-xxl-4 mt-1 mt-sm-0">
+                        <div class="row row-cols-auto mt-3 mt-sm-0 mt-lg-2">
                             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 106 100" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" alt="">
                                 <use xlink:href="..\Imagenes\start\check.svg#Layer_1" />
                             </svg>
@@ -167,7 +164,65 @@
             </div>
 
             <div class="bg-container bg-whithe">
-                <img id="androidApp" class="" src="" alt="" />
+                <img id="androidApp" class="background" src="" alt="" />
+                <div class="row py-3 py-sm-4 py-lg-5 px-0 my-5 justify-content-center">
+                    <span class="row p-0 m-0 pe-lg-5 col-auto ps-lg-2 ps-xl-0 justify-content-center pt-2 pt-md-0">
+                        <div class="shadow-css androidApp bg-content bg-white col-auto px-0 px-sm-4 py-4 py-md-5 me-lg-3 ms-md-2 ms-lg-0">
+                            <h2 class="text-start px-4 px-md-3 mt-1">Consultation Android application</h2>
+                            <p class="px-4 px-md-3 mb-sm-3 mb-md-4">to make faster data-driven decisions.</p>
+                        </div>
+                        <span class="col-auto mx-3 mx-lg-2 mx-xl-5 px-xl-3 d-none d-md-inline"></span>
+                        <span class="w-100 d-md-none"></span>
+                        <span class="check-list second-list bg-content col-auto mt-3 mt-sm-4 mt-md-0 pe-0 pe-sm-2">
+                            <div class="row row-cols-auto mt-4">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 106 100" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" alt="">
+                                    <use xlink:href="..\Imagenes\start\check.svg#Layer_1" />
+                                </svg>
+                                <p>Access to information at your fingertips from anywhere.</p>
+                            </div>
+                            <div class="row row-cols-auto">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 106 100" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" alt="">
+                                    <use xlink:href="..\Imagenes\start\check.svg#Layer_1" />
+                                </svg>
+                                <p>Make immediate and updated queries directly from the source.</p>
+                            </div>
+                            <div class="row row-cols-auto pb-0">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 106 100" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" alt="">
+                                    <use xlink:href="..\Imagenes\start\check.svg#Layer_1" />
+                                </svg>
+                                <p>Provide relevant information for planning and decision-making.</p>
+                            </div>
+                        </span>
+                        <span class="col-auto p-0 ms-2 ms-xxl-1 me-xxl-3 d-none d-xl-inline"></span>
+                    </span>
+                    <span class="row p-0 m-0 justify-content-center py-2 py-md-0 px-2 px-sm-0">
+                        <h2 class="text-center my-3 my-sm-4 my-md-5">Tech stack</h2>
+                        <span class="row p-0 m-0 col-auto justify-content-center bg-content gap-md-1 gap-lg-4 gap-xl-5 mt-4 mt-md-0">
+                            <div class="shadow-css tech-stack bg-white col-auto p-4 pt-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon mt-4 mb-2" alt="">
+                                    <path d="M4.08 5.227A3 3 0 0 1 6.979 3H17.02a3 3 0 0 1 2.9 2.227l2.113 7.926A5.228 5.228 0 0 0 18.75 12H5.25a5.228 5.228 0 0 0-3.284 1.153L4.08 5.227Z" />
+                                    <path fill-rule="evenodd" d="M5.25 13.5a3.75 3.75 0 1 0 0 7.5h13.5a3.75 3.75 0 1 0 0-7.5H5.25Zm10.5 4.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm3.75-.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" clip-rule="evenodd" />
+                                </svg>
+                                <p class="px-2 m-0">PHP, MySQL, FPDF, JSON, XML, CSV & PHPMailer.</p>
+                            </div>
+                            <span class="my-3 pt-1 d-sm-none"></span>
+                            <div class="shadow-css tech-stack bg-white col-auto p-4 pt-2 mx-lg-3 mx-xl-2 mx-xxl-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon mt-4 mb-2" alt="">
+                                    <path fill-rule="evenodd" d="M2.25 5.25a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3V15a3 3 0 0 1-3 3h-3v.257c0 .597.237 1.17.659 1.591l.621.622a.75.75 0 0 1-.53 1.28h-9a.75.75 0 0 1-.53-1.28l.621-.622a2.25 2.25 0 0 0 .659-1.59V18h-3a3 3 0 0 1-3-3V5.25Zm1.5 0v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5Z" clip-rule="evenodd" />
+                                </svg>
+                                <p class="px-2 m-0">HTML, CSS, JavaScript, JQUERY, AJAX, Chart.js & Bootstrap.</p>
+                            </div>
+                            <span class="my-3 pt-1 my-md-3 pt-md-0 d-lg-none"></span>
+                            <div class="shadow-css tech-stack bg-white col-auto p-4 pt-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon p-1 mt-4 mb-2" alt="">
+                                    <path d="M10.5 18.75a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" />
+                                    <path fill-rule="evenodd" d="M8.625.75A3.375 3.375 0 0 0 5.25 4.125v15.75a3.375 3.375 0 0 0 3.375 3.375h6.75a3.375 3.375 0 0 0 3.375-3.375V4.125A3.375 3.375 0 0 0 15.375.75h-6.75ZM7.5 4.125C7.5 3.504 8.004 3 8.625 3H9.75v.375c0 .621.504 1.125 1.125 1.125h2.25c.621 0 1.125-.504 1.125-1.125V3h1.125c.621 0 1.125.504 1.125 1.125v15.75c0 .621-.504 1.125-1.125 1.125h-6.75A1.125 1.125 0 0 1 7.5 19.875V4.125Z" clip-rule="evenodd" />
+                                </svg>
+                                <p class="px-2 m-0 text-center mb-1 mb-sm-0 pb-3 pb-sm-0">Android, Volley & MPAndroidChart.</p>
+                            </div>
+                        </span>
+                    </span>
+                </div>
             </div>
         </div>
 
