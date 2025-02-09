ebe780b93308f4e917bd7fdb968360e3b7cee567
annetvd <132998548+annetvd@users.noreply.github.com>
2025-02-09 00:43:41 -0600

    Set up conditional environment variable loading based on environment

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,7 @@
 const mysql = require("mysql2/promise");
-require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+    require("dotenv").config();
+}
 
 //awococado
 async function connectAw() {
